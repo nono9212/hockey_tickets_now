964254087439ba121e8ce75de2f26759a27caa32
Arnaud Chol <arnaud.chol@gmail.com>
2021-10-03 17:26:45 -0400

    added images

--- CentreBell.py
+++ CentreBell.py
@@ -4,3 +4,12 @@ Created on Sun Oct  3 17:17:09 2021
 
 @author: arnau
 """
+
+import matplotlib.pyplot as plt
+
+class CentreBell:
+    
+    def plotStadium():
+        stadium = plt.imread('./img/stadium.png')
+        fig, ax = plt.subplots()
+        im      = ax.imshow(stadium)
