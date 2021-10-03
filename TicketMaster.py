990f6efc09ffc61db669076b5a2b878a3ccd0311
Arnaud Chol <arnaud.chol@gmail.com>
2021-10-03 18:14:33 -0400

    change to support centrebell notations

--- TicketMaster.py
+++ TicketMaster.py
@@ -9,23 +9,19 @@ class TicketMaster:
     
     def __init__(self):
         try:
-            
-            self.placeInfoDict = np.load('data/placeInfoDict.npy',allow_pickle='TRUE').item()
             self.IDToPlaceName = np.load('data/IDToPlaceName.npy',allow_pickle='TRUE').item()
 
         except:
-            self.placeInfoDict, self.IDToPlaceName = self.getAllSeatsDict()
+            self.IDToPlaceName = self.getAllSeatsDict()
             if(not os.path.exists("data")):
                 os.makedirs('data')
-            np.save('data/placeInfoDict.npy', self.placeInfoDict) 
             np.save('data/IDToPlaceName.npy', self.IDToPlaceName) 
     
     def getAllSeatsDict(self, gameID = '31005B2A0984366E'):
         
         urlPlace = "https://mapsapi.tmol.io/maps/geometry/3/event/"+gameID+"/placeDetailNoKeys?systemId=HOST&useHostGrids=true&app=CCP&sectionLevel=true"
         rplace = json.loads(requests.get(urlPlace).text)['pages'][0]["segments"]
-        
-        placeDict = {}
+
         IDToPlaceName = {}
         for section in rplace:
             for row in section['segments'][0]['segments']:
@@ -33,15 +29,10 @@ class TicketMaster:
                     ID = seat[0]
                     ligne = row['name']
                     colonne = seat[1]
-                    coords = [seat[2], seat[3]]
                     section_name = section['name']
-                    placeDict[section_name+'-'+ligne+colonne] = {'ticketMasterID':ID,
-                                                                 'section': section_name,
-                                                                 'row': ligne,
-                                                                 'colonne':colonne,
-                                                                 'coords': coords}
+
                     IDToPlaceName[ID] = section_name+'-'+ligne+colonne
-        return placeDict, IDToPlaceName
+        return IDToPlaceName
     
 
     def getNextMaches(self, ):
@@ -94,7 +85,7 @@ class TicketMaster:
         
         listPlaces = []
         for text in internal:
-            r = TicketMaster.seatKeysToArray(text, ante = ante)
+            r = TicketMaster.seatKeysToArray(self, text, ante = ante)
             if(isinstance(r, list)):
                 listPlaces += r
             else:
@@ -109,33 +100,14 @@ class TicketMaster:
         urlPrice = "https://offeradapter.ticketmaster.com/api/ismds/event/"+gameID+"/facets?show=totalpricerange+places&by=offers&oq=not(locked)&q=available&apikey=b462oi7fic6pehcdkzony5bxhe&apisecret=pquzpfrfz7zd2ylvtz3w5dtyse&resaleChannelId=internal.ecommerce.consumer.desktop.web.browser.ticketmaster.ca"
         r2 = json.loads(requests.get(urlPrice).text)['facets']
         
-        urlPlace = "https://mapsapi.tmol.io/maps/geometry/3/event/"+gameID+"/placeDetailNoKeys?systemId=HOST&useHostGrids=true&app=CCP&sectionLevel=true"
-        rplace = json.loads(requests.get(urlPlace).text)['pages'][0]["segments"]
         
-        placeDict = {}
-        for p in rplace:
-            section = p['name']
-            for row in p['segments']:
-                for w in row['segments']:
-                    rowLetter = w['name']
-                    for seat in w['placesNoKeys']:
-                        key = seat[0]
-                        number = seat[1]
-                        xloc = seat[2]
-                        yloc = seat[3]
-                        placeDict[key] = {"section": section,
-                                          "row": rowLetter,
-                                          "number": number,
-                                          "coords": [xloc,yloc]}
-        
-        availableTickets = []
+        availableTickets = {}
         for ticketOffer in r2:
             if(len(ticketOffer['offers'])>0):
-                seatsIDs = TicketMaster.seatKeysToArray(ticketOffer["places"][0])
+                seatsIDs = TicketMaster.seatKeysToArray(self, ticketOffer["places"][0])
                 for s in seatsIDs:
-                    seatInfo = placeDict[s]
-                    seatInfo['price'] = ticketOffer['totalPriceRange'][0]['max']
-                    availableTickets += [seatInfo]
+                    seatInfo = self.IDToPlaceName[s]
+                    availableTickets[seatInfo] = ticketOffer['totalPriceRange'][0]['max']
             
         return availableTickets
 
