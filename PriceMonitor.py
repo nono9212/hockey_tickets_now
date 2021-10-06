5f01789e90ab7ff57d3770ebc95c6d895fe83673
Arnaud Chol <arnaud.chol@gmail.com>
2021-10-06 17:36:40 -0400

    add next matches updator

--- PriceMonitor.py
+++ PriceMonitor.py
@@ -4,7 +4,29 @@ Created on Wed Oct  6 17:21:36 2021
 
 @author: arnau
 """
+from TicketMaster import TicketMaster
+import datetime
+import numpy as np
+import os
+
+
 class PriceMonitor:
     
-    def __init__():
-        pass
+    
+    
+    def __init__(self):
+        self.tm = TicketMaster()
+        
+    def updateNextMatches(self):
+        date = datetime.date.today().strftime("%Y-%m-%d")
+        try:
+            self.nextMatches = np.load('data/nextMatches.npy',allow_pickle='TRUE').item()[date]
+        except:
+            print("No files found which match up to date match status, going to scrap it")
+            self.nextMatches = self.tm.getNextMaches()
+            if(not os.path.exists("data")):
+                os.makedirs('data')
+            np.save('data/nextMatches.npy', {date:self.nextMatches}) 
+        
+        
+        
